c86da5ac1389aa57e31e1fe5042834226b7d29c7
L. Theodore Obonye <ltobonye@gmail.com>
2021-02-27 19:51:20 +0200

    check visits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ActivatedRouteSnapshot, Router } from '@angular/router';
+import { url } from 'inspector';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'crowfunding';
   count = 0;
   path: string;
-  constructor(private router: Router, private activatedRoute: ActivatedRoute) {
-    console.log(activatedRoute.snapshot.url);
-    this.path = activatedRoute.snapshot.url[0].path;
-    if (this.path === '') {
-      this.count++;
-    } // array of states
-    console.log(activatedRoute.snapshot.url[0].path); }
+  url: Observable<string>;
+  constructor(private router: Router, private route: ActivatedRoute) {
+    const id: Observable<string> = route.params.pipe(map(p => p.id));
+    this.url = route.url.pipe(map(segments => segments.join('')));
+  }
+
+  ngOnInit() {
+  const urld = this.url.subscribe(d => d ? this.count++ : null);
+  console.log(urld);
+
+  }
+
 }
