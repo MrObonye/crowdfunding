74f08f44135388b8e246f2d25c889c805fe46196
L. Theodore Obonye <ltobonye@gmail.com>
2021-02-27 19:56:06 +0200

    check visits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,20 +8,11 @@ import { map } from 'rxjs/operators';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit{
   title = 'crowfunding';
   count = 0;
-  path: string;
-  url: Observable<string>;
-  constructor(private router: Router, private route: ActivatedRoute) {
-    const id: Observable<string> = route.params.pipe(map(p => p.id));
-    this.url = route.url.pipe(map(segments => segments.join('')));
-  }
-
   ngOnInit() {
-  const urld = this.url.subscribe(d => d ? this.count++ : null);
-  console.log(urld);
-
+    this.count++;
   }
 
 }
