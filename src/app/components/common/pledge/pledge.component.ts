698712a5eb4d14e1b6fcb390ade30e54365bf2a5
L. Theodore Obonye <ltobonye@gmail.com>
2021-03-02 20:36:00 +0200

    thank you message submitted

--- src/app/components/common/pledge/pledge.component.ts
+++ src/app/components/common/pledge/pledge.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { ModalService } from '../../modal/modal.service';
 
 @Component({
   selector: 'app-pledge',
@@ -16,12 +17,24 @@ export class PledgeComponent implements OnInit {
   @Input() selected: number;
 
   @Output() enableBorder = new EventEmitter();
-  constructor() { }
+  constructor(private modalService: ModalService) { }
 
   ngOnInit(): void {
   }
   select(value) {
     this.enableBorder.emit(value);
 
+  }
+  openModal(id: string): void {
+    console.log('tset');
+
+    this.modalService.close('custom-modal-1');
+    this.modalService.open(id);
+  }
+  closeModal(id: string): void {
+    this.modalService.close(id);
+  }
+  submit(){
+    this.openModal('custom-modal-2');
   }
 }
