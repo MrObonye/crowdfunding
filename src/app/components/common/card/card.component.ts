029734f307bed4e2a605064f8862e63e65ba950a
L. Theodore Obonye <ltobonye@gmail.com>
2021-03-03 16:07:18 +0200

    fixed responsiveness to medium views

--- src/app/components/common/card/card.component.ts
+++ src/app/components/common/card/card.component.ts
@@ -4,17 +4,25 @@ import { Component, Input, OnInit } from '@angular/core';
   selector: 'app-card',
   template: `
     <div class="p-4" >
-  <h5 class="card-title">{{ cardTitle }}</h5>
-  <h5 class="card-sub-title">{{cardSubTitle}}</h5>
+      <div class="row">
+      <h5 class="card-title col-lg-6">{{ cardTitle }}</h5>
+  <h5 class="card-sub-title col-lg-6">{{cardSubTitle}}</h5>
+      </div>
+
   <div class="card-text">{{cardText}}</div>
-  <div class="card-digits row m-0">
+  <div class="row">
+  <div class="card-digits row col-lg-6 m-0">
       <h1 class="card-digits-number">{{cardNumber}}</h1>
       <p class="card-digits-text p-2 m-1">left</p>
   </div>
-    <app-rectangular-button class="mb-3"
+  <div class="col-lg-6">
+  <app-rectangular-button class="mb-3"
     [buttonName]="buttonName"
     [buttonType]="buttonType">
     </app-rectangular-button>
+  </div>
+
+  </div>
   </div>`,
 })
 export class CardComponent implements OnInit {
